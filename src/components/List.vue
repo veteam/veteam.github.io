d24cb98e6238d6e3f7dfbe23a9931e65e2209190
Vetea <vetea12@hotmail.fr>
2022-12-06 09:21:43 +0100

    add start script

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,66 +1,61 @@
 <template lang="">
 
-<div class="relative">
-  <label class="sr-only"> Category </label>
+<div class="relative mt-10">
+  <label class="sr-only"> Categorie ou titre ou contenu </label>
 
   <input
     class="w-full py-3 pl-3 pr-12 text-sm border-2 border-gray-200 rounded text-gray-800 "
     type="text"
-    placeholder="Category"
-    v-on:keyup.enter="loadCardsFromCategory"
+    placeholder="Categorie ou titre ou contenu..."
     v-model="search"
+    v-on:input="loadCardsFromCategory"
   />
+</div>
 
-  <span class="absolute text-gray-500 -translate-y-1/2 pointer-events-none top-1/2 right-4">
-     <svg
-      class="w-3 h-3 sm:w-6 sm:h-6"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-      ></path>
-    </svg>
+<a class="mt-8 inline-block p-[2px] rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" target="_blank" rel="noopener noreferrer" href="https://vetea.synology.me/wordpress/wp-admin/post-new.php">
+  <span class="block px-8 py-3 text-large font-medium  bg-gray-900 rounded-full hover:bg-transparent">
+    Ajouter une carte
   </span>
-</div>
+</a>
+
+<a class="sm:ml-8  mt-8 inline-block p-[2px] rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 hover:text-white active:text-opacity-75 focus:outline-none focus:ring" target="_blank" rel="noopener noreferrer" href="https://vetea.synology.me/wordpress/wp-admin/edit-tags.php?taxonomy=category">
+  <span class="block px-8 py-3 text-large font-medium  bg-gray-900 rounded-full hover:bg-transparent">
+    Ajouter une catégorie
+  </span>
+</a>
 
 
-<div v-if="research==false" class="grid grid-flow-col p-6 gap-4">
+<div v-if="research==false" class="grid md:grid-cols-3 gap-4 mt-10 w-full h-full">
     <div  v-for="(card, index) in cards" :key="index">
-       <div class="p-1  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl w-96 m-0">
-            <a class="block p-6 bg-gray-900 sm:p-8 rounded-xl" href="">
-                <h5 class="text-xl font-bold text-white">{{ card.title.rendered }}</h5>
-                <p class="mt-2 text-sm text-gray-400 mb-2" v-html="card.content.rendered"></p>
-                    <li v-for="(category, cat) in card.meta" :key="cat" class="inline-block rounded-full text-white text-xs font-medium px-3 py-1.5 bg-gray-800 m-1">
+       <div class="p-1  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl m-0 h-full">
+            <div class="block p-6 bg-gray-900 sm:p-8 rounded-xl h-full">
+                <h5 class="lg:text-xl text-xs font-bold text-white">{{ card.title.rendered }}</h5>
+                <p class="mt-2 text-sm text-gray-400 mb-8" v-html="card.content.rendered"></p>
+                    <li v-for="(category, cat) in card.meta" :key="cat" class="inline-block rounded-full text-white text-sm md:text-large md:font-medium px-3 py-1.5 bg-gray-800 m-1">
                         {{category}}
                     </li>
-            </a>
-            <p class="p-6 text-xl">{{ card.modified }}</p>
+            <p class="p-3 text-sm">{{ card.modified }}</p>
+            <p class="p-3 text-sm">{{ card.date }}</p>
+            </div>
         </div>
     </div>
 </div>
 
-
-<div v-else class="grid grid-flow-col p-6 gap-4">
+<div v-else class="grid md:grid-cols-2 gap-4 mt-10 w-full h-full">
     <div  v-for="(card, index) in result" :key="index">
-       <div class="p-1  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl w-96 m-0">
-            <a class="block p-6 bg-gray-900 sm:p-8 rounded-xl" href="">
-                <h5 class="text-xl font-bold text-white">{{ card.title.rendered }}</h5>
+       <div class="p-1  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl w-96 m-0 w-full h-full">
+            <a class="block p-6 bg-gray-900 sm:p-8 rounded-xl h-full" href="">
+                <h5 class="lg:text-xl text-xs font-bold text-white">{{ card.title.rendered }}</h5>
                 <p class="mt-2 text-sm text-gray-400 mb-2" v-html="card.content.rendered"></p>
                     <li v-for="(category, cat) in card.meta" :key="cat" class="inline-block rounded-full text-white text-xs font-medium px-3 py-1.5 bg-gray-800 m-1">
                         {{category}}
                     </li>
+            <p class="p-3 text-sm">{{ card.modified }}</p>
+            <p class="p-3 text-sm">{{ card.date }}</p>
             </a>
-            <p class="p-6 text-xl">{{ card.modified }}</p>
         </div>
     </div>
 </div>
-
 
 </template>
 <script>
@@ -79,11 +74,16 @@ export default {
         this.loadAllCards();
     },
     methods: {
+        changeDateFormat(time, title){
+            let temp = time.split("-")
+            temp[2] = temp[2].split('T');
+            return(title + " : " + temp[2][0] + '-' + temp[1] + '-' + temp[0] + ' | ' + temp[2][1])
+        },
         async loadAllCards(){
-            const fetched_cards = await fetch("http://vetea.synology.me/wordpress/wp-json/wp/v2/posts/");
+            const fetched_cards = await fetch("https://vetea.synology.me/wordpress/wp-json/wp/v2/posts/");
             this.cards = await fetched_cards.json();
 
-            const fetched_category = await fetch("http://vetea.synology.me/wordpress/wp-json/wp/v2/categories/");
+            const fetched_category = await fetch("https://vetea.synology.me/wordpress/wp-json/wp/v2/categories/");
             this.categories = await fetched_category.json();
 
             this.categories.forEach(element_categories => {
@@ -91,19 +91,16 @@ export default {
                     if (element_cards.categories.includes(element_categories.id))
                     {
                         element_cards.meta.push(element_categories.name)
-                        console.log(element_cards, element_categories)
                     }
                 })
             })
 
             this.cards.forEach(element_cards => {
-                let temp = element_cards.modified.split("-")
-                temp[2] = temp[2].split('T');
-                element_cards.modified = "Modification : " + temp[2][0] + '-' + temp[1] + '-' + temp[0] + ' | ' + temp[2][1]
+                element_cards.modified = this.changeDateFormat(element_cards.modified, "Modification")
+                element_cards.date = this.changeDateFormat(element_cards.date, "Création")                
             })
         },
         loadCardsFromCategory(){
-            console.log(this.search)
             if (this.search == "")
             this.research = false
             else
@@ -111,20 +108,19 @@ export default {
                 this.research = true
                 this.result = [];
                 this.cards.forEach(element_cards => {
-                    if (element_cards.meta.includes(this.search))
+                    if (element_cards.meta.includes(this.search) || element_cards.title.rendered.includes(this.search) || element_cards.content.rendered.includes(this.search))
                         {
                             this.result.push(element_cards)
                         }
                 })
             }
-
         }
     },
-
     async beforeCreate() {
         loadAllCards()
     },
 }
 </script>
 <style lang="">
+
 </style>
